076fb46cb444e0946884e955498a009fc3bb81f1
hal01001k <hiteshdaksh@yahoo.co.uk>
2020-07-15 19:22:26 +0530

    Leaders and promotions router

--- index.js
+++ index.js
@@ -6,14 +6,20 @@ const bodyParser = require('body-parser')
 const hostname = 'localhost';
 const port = 3000;
 const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
+
+
 const app = express();
-
-
-
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/dishes', dishRouter);
+app.use('/dishes/:dishId', dishRouter);
+app.use('/leaders', leaderRouter);
+app.use('/leaders/:leaderId', leaderRouter);
+app.use('/promotions', promoRouter);
+app.use('/promotions/:promoId', promoRouter);
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const dishRouter = express.Router();
+const dishRouter = express.Router({ mergeParams: true });
 
 dishRouter.use(bodyParser.json());
 
@@ -12,17 +12,26 @@ dishRouter.route('/')
         next();
     })
     .get((req,res,next) => {
-        res.end('Will send all the dishes to you!');
+        if(req.params.dishId != null)
+        res.end('Will send the dish '+ req.params.dishId + ' to you!');
+        else
+            res.end('Will send all the dishes to you!');
     })
     .post((req, res, next) => {
+        if(req.params.dishId != null)
         res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+        else
+        res.end('Will add the dish: ' + req.params.dishId + ' with details: ' + req.body.description);
     })
     .put((req, res, next) => {
         res.statusCode = 403;
         res.end('PUT operation not supported on /dishes');
     })
     .delete((req, res, next) => {
-        res.end('Deleting all dishes');
+        if(req.params.dishId != null)
+        res.end('Deleting dish ' + req.params.dishId +' as per request');
+        else
+            res.end('Deleting all the dishes as per request');
     });
 
 module.exports = dishRouter;
